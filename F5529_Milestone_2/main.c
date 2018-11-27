b90062f3a8dec5d8d6c9d92945c45e66f0ed8c52
ccraig427 <35497783+ccraig427@users.noreply.github.com>
2018-11-27 16:43:08 -0500

    Update main.c

--- F5529_Milestone_2/main.c
+++ F5529_Milestone_2/main.c
@@ -1,80 +1,32 @@
-/* --COPYRIGHT--,BSD_EX
- * Copyright (c) 2012, Texas Instruments Incorporated
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * *  Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- * *  Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * *  Neither the name of Texas Instruments Incorporated nor the names of
- *    its contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
- * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
- * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- *******************************************************************************
- *
- *                       MSP430 CODE EXAMPLE DISCLAIMER
- *
- * MSP430 code examples are self-contained low-level programs that typically
- * demonstrate a single peripheral function or device feature in a highly
- * concise manner. For this the code may rely on the device's power-on default
- * register values and settings such as the clock configuration and care must
- * be taken when combining code from several examples to avoid potential side
- * effects. Also see www.ti.com/grace for a GUI- and www.ti.com/msp430ware
- * for an API functional library-approach to peripheral configuration.
- *
- * --/COPYRIGHT--*/
-//******************************************************************************
-//   MSP430F552x Demo - ADC12, Sample A0, Set P1.0 if A0 > 0.5*AVcc
-//
-//   Description: A single sample is made on A0 with reference to AVcc.
-//   Software sets ADC12SC to start sample and conversion - ADC12SC
-//   automatically cleared at EOC. ADC12 internal oscillator times sample (16x)
-//   and conversion. In Mainloop MSP430 waits in LPM0 to save power until ADC12
-//   conversion complete, ADC12_ISR will force exit from LPM0 in Mainloop on
-//   reti. If A0 > 0.5*AVcc, P1.0 set, else reset.
-//
-//                MSP430F552x
-//             -----------------
-//         /|\|                 |
-//          | |                 |
-//          --|RST              |
-//            |                 |
-//     Vin -->|P6.0/CB0/A0  P1.0|--> LED
-//
-//   Bhargavi Nisarga
-//   Texas Instruments Inc.
-//   April 2009
-//   Built with CCSv4 and IAR Embedded Workbench Version: 4.21
-//******************************************************************************
-
 #include <msp430.h>
 #include <math.h>
-float steady;
+
+float current_TEMP;
 float ADC_Voltage;
 float ADC_Temp;
-int Temp;
+const perBit = (3.3/4096);
+float desired_TEMP;
+
 void ADC_Setup();
 void PWM_Setup();
 void input_TEMP();
+void UART_Setup();
+
+void UART_Setup()
+{
+    P3SEL |= BIT3+BIT4;                       // P3.3,4 = USCI_A0 TXD/RXD
+    UCA0CTL1 |= UCSWRST;                      // **Put state machine in reset**
+    UCA0CTL1 |= UCSSEL_2;                     // SMCLK
+    // Baud Rate calculation
+    // 1000000/9600 = 104.1667
+    // Fractional portion = 0.1667
+    // Use Table 24-5 in Family User Guide
+    UCA0BR0 = 104;                             // 1MHz 115200 (see User's Guide)
+    UCA0BR1 = 0;                              // 1MHz 115200
+    UCA0MCTL |= UCBRS_3 + UCBRF_0;            // Modulation UCBRSx=1, UCBRFx=0
+    UCA0CTL1 &= ~UCSWRST;                     // **Initialize USCI state machine**
+    UCA0IE |= UCTXIE;                         // Enable USCI_A0 TX interrupt
+}
 
 void ADC_Setup()
 {
@@ -83,7 +35,6 @@ void ADC_Setup()
     ADC12IE = 0x01;                           // Enable interrupt
     ADC12CTL0 |= ADC12ENC;
     P6SEL |= 0x01;                            // P6.0 ADC option select
-    P1DIR |= 0x01;
 }
 
 void PWM_Setup()
@@ -92,7 +43,7 @@ void PWM_Setup()
     P2SEL |= BIT0; //2.0 TA1
     TA1CTL = TASSEL_2 + MC_1 + TACLR; // SMCLK, up-down mode, clear
     TA1CCR0 = 500; // PWM Period
-    TA1CCR1 = 0; // CCR1 PWM duty cycle
+    TA1CCR1 = 1; // CCR1 PWM duty cycle
     TA1CCTL1 = OUTMOD_7; // CCR1 toggle/set
 }
 
@@ -100,20 +51,25 @@ void PWM_Setup()
 void inputTEMP()
 {
     ADC_Temp = (ADC_Voltage - .424)/.00625;
-    Temp = (int)ADC_Temp;
+    current_TEMP = (int)ADC_Temp;
+}
+
+void compareTEMP()
+{
+
 }
 
 int main(void)
 {
     WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
+    UART_Setup();
     PWM_Setup();
     ADC_Setup();
     while (1)
     {
         ADC12CTL0 |= ADC12SC;                   // Start sampling/conversion
-        ADC_Voltage = ADC12MEM0;
-        inputTEMP();
-        __bis_SR_register(LPM0_bits + GIE);
+        ADC_Voltage = ADC12MEM0 * perBit;
+        __bis_SR_register(GIE);
     }
 }
 
@@ -127,11 +83,8 @@ __interrupt void ADC12_ISR(void)
   case  2: break;                           // Vector  2:  ADC overflow
   case  4: break;                           // Vector  4:  ADC timing overflow
   case  6:                                  // Vector  6:  ADC12IFG0
-    if (ADC12MEM0 >= 0x7ff)                 // ADC12MEM = A0 > 0.5AVcc?
-      P1OUT |= BIT0;                        // P1.0 = 1
-    else
-      P1OUT &= ~BIT0;                       // P1.0 = 0
-
+    inputTEMP();
+    compareTEMP();
     __bic_SR_register_on_exit(LPM0_bits);   // Exit active CPU
     break;
   case  8: break;                           // Vector  8:  ADC12IFG1
@@ -151,3 +104,27 @@ __interrupt void ADC12_ISR(void)
   default: break;
   }
 }
+
+#pragma vector=USCI_A0_VECTOR
+__interrupt void USCI_A0_ISR(void)
+{
+  switch(__even_in_range(UCA0IV,4))
+  {
+  case 0:break;                             // Vector 0 - no interrupt
+  case 2:                                   // Vector 2 - RXIFG
+    while (!(UCA0IFG & UCTXIFG));             // USCI_A0 TX buffer ready?
+    UCA0IFG &= ~UCTXIFG; //Clears TX flag interrupt
+    UCA0TXBUF = ADC_Temp;                  //Current temp read from Tx buffer
+    break;
+  case 4:break;                             // Vector 4 - TXIFG
+  default: break;
+
+  if (UCA0IFG & UCRXIFG)
+  {
+      UCA0IFG &= ~UCRXIFG;
+      UCA0RXBUF = desired_TEMP;
+  }
+
+  }
+}
+
